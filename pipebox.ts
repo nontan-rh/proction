aecdcced70e196b795e3b89f7b44a9780cf55d95
nontan-rh <niszkml@gmail.com>
2024-04-02 01:52:59 +0900

    Fix pipebox

--- pipebox.ts
+++ pipebox.ts
@@ -1,30 +1,76 @@
 import { Box } from "./box.ts";
 
-export function pipeBox<T>(): [PipeBoxReader<T>, PipeBoxWriter<T>] {
+export function pipeBox<T>(): [IPipeBoxR<T>, IPipeBoxW<T>] {
   const box = new Box<T>();
-  return [new PipeBoxReader(box), new PipeBoxWriter(box)];
+  return [new PipeBoxR(box), new PipeBoxW(box)];
 }
 
-export class PipeBoxWriter<T> {
+export function pipeBoxR<T>(value: T): IPipeBoxR<T> {
+  const box = new Box<T>();
+  box.value = value;
+  return new PipeBoxR(box);
+}
+
+export function pipeBoxRW<T>(): IPipeBoxRW<T> {
+  const box = new Box<T>();
+  return new PipeBoxRW(box);
+}
+
+export interface IPipeBoxW<T> {
+  setValue(v: T): void;
+  clear(): void;
+}
+
+export interface IPipeBoxR<T> {
+  getValue(): T;
+}
+
+export interface IPipeBoxRW<T> extends IPipeBoxR<T>, IPipeBoxW<T> {}
+
+class PipeBoxW<T> implements IPipeBoxW<T> {
   #box: Box<T>;
 
   constructor(box: Box<T>) {
     this.#box = box;
   }
 
-  set value(v: T) {
+  setValue(v: T): void {
     this.#box.value = v;
   }
+
+  clear(): void {
+    this.#box.clear();
+  }
 }
 
-export class PipeBoxReader<T> {
+class PipeBoxR<T> implements IPipeBoxR<T> {
   #box: Box<T>;
 
   constructor(box: Box<T>) {
     this.#box = box;
   }
 
-  get value(): T {
+  getValue(): T {
     return this.#box.value;
+  }
+}
+
+class PipeBoxRW<T> implements IPipeBoxRW<T> {
+  #box: Box<T>;
+
+  constructor(box: Box<T>) {
+    this.#box = box;
+  }
+
+  getValue(): T {
+    return this.#box.value;
+  }
+
+  setValue(v: T): void {
+    this.#box.value = v;
+  }
+
+  clear(): void {
+    this.#box.clear();
   }
 }
